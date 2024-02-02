b1e2d1ba3238ec31dfa207e0434315b524af6a47
scottsak <48599109+scottsak@users.noreply.github.com>
2024-02-01 19:37:23 -0800

    v7p4

--- src/components/LoseScreen.jsx
+++ src/components/LoseScreen.jsx
@@ -52,7 +52,7 @@ function LoseScreen(props) {
 
   const shareScore = React.useCallback(async () => {
     // let textShare = 'SoundTracking: ⭐' + props.score + '⭐\nPlaylist:\n\n';
-    let textShare = 'SoundTracking ' + props.score;
+    let textShare = 'SoundTracking ' + props.score + '\nPlaylist:\n\n';
     setCopied(true);
     setTimeout(function () {
       setCopied(false);
